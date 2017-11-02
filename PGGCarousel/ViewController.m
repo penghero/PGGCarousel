b6af342886901c01bf0cd8b1d7eba6b473249601
penghero <chen@chenpengs-MacBook-Air.local>
2017-11-02 09:02:44 +0800

    添加GitHub地址

--- PGGCarousel/ViewController.m
+++ PGGCarousel/ViewController.m
@@ -4,7 +4,7 @@
 //
 //  Created by 陈鹏 on 2017/10/31.
 //  Copyright © 2017年 penggege.CP. All rights reserved.
-//
+//GitHub 地址 https://github.com/penghero/PGGCarousel.git
 
 #import "ViewController.h"
 #import "PGGCarouselView.h"
